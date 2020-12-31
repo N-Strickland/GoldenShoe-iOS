b0401e02af3bb1913ce26924ba41c05e24011d81
Nathan Strickland <services+github@nstrickland.com>
2020-12-31 17:54:37 +0000

    Initial Commit

---
+++ GoldenShoe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoldenShoe
+//
+//  Created by Nathan Strickland on 31/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
